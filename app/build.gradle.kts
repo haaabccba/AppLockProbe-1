ef21e7def4fb79184c186ea0c97f60f2df4d84fe
haaabccba <haaabccba@gmail.com>
2026-07-28 10:46:21 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,42 @@
 
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.hyperprobe"
+
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.hyperprobe"
+        minSdk = 28
+        targetSdk = 36
+
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+dependencies {
+    compileOnly("io.github.libxposed:api:102")
+}
