751e0fd35e91d85419e06203030e2dabb97a3e09
haaabccba <haaabccba@gmail.com>
2026-07-28 10:35:09 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://api.xposed.info/")
+    }
+}
+
+rootProject.name = "AppLockProbe"
+
+include(":app")
